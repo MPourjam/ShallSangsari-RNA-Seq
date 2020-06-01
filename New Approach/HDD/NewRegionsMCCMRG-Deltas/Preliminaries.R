0d96306c1000ac07b8bb3f6a70e05d1fd993eb11
Mohsen Pourjam <33843138+MPourjam@users.noreply.github.com>
2020-06-01 16:12:00 +0430

    Update Preliminaries.R
    Note line 21 comment

--- New Approach/HDD/NewRegionsMCCMRG-Deltas/Preliminaries.R
+++ New Approach/HDD/NewRegionsMCCMRG-Deltas/Preliminaries.R
@@ -18,7 +18,7 @@ MCC <- c(seq(1,20,0.5))
 MRG <- c(seq(10,100, 10))
 MCC_MRG_Grid <- expand.grid(MCC,MRG)
 if (length(fullCov_Path) == 1){
-if (str_detect(fullCov_Path,".*.rds$")){  ##########?????? Do we really need fullCov for this step
+if (str_detect(fullCov_Path,".*.rds$")){  ###### What if therea are more than one file containing "fullcov" and ".rds
   load(fullCov_Path)
 } else if(stringr::str_detect(fullCov_Path, ".*.RData")) {
   fullCov <<- load(fullCov_Path)
